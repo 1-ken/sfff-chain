557d02e467867c253acdf018ccde9dab056bba9e
KEN <kennicmwimu@gmail.com>
2024-10-26 11:17:56 +0300

    created validation function and did unit testing on  it.

--- block.js
+++ block.js
@@ -1,33 +1,36 @@
-const SHA256 =require('crypto-js/sha256')
+const SHA256 = require("crypto-js/sha256");
 
-class Block{
-    constructor(timestamp,lastHash,hash,data){
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
-    }
-    toString(){
-        return `
+class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+  toString() {
+    return `
         TimeStamp:${this.timestamp}
-        LastHash:${this.lastHash.substring(0,10)}
-        Hash:${this.hash.substring(0,10)}
+        LastHash:${this.lastHash.substring(0, 10)}
+        Hash:${this.hash.substring(0, 10)}
         Data: ${this.data}
-        `
-    }
-    static Genesis(){
-        return new this('genesis time','-------','f1r57-h145h',[])
-    }
-    static hash(timestamp,lastHash,data){
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
-    }
+        `;
+  }
+  static Genesis() {
+    return new this("genesis time", "-------", "f1r57-h145h", []);
+  }
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
+  static blockHash(block) {
+    const { timestamp, lastHash, data } = block;
+    return Block.hash(timestamp, lastHash, data);
+  }
 
-    static mineBlock(lastBlock,data){
-        const timestamp = Date.now();
-        const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp,lastHash,data);
-        return new this(timestamp,lastHash,hash,data)
-    }
-    
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+    return new this(timestamp, lastHash, hash, data);
+  }
 }
 module.exports = Block;
--- blockchain.test.js
+++ blockchain.test.js
@@ -1,9 +1,10 @@
 const Blockchain = require("./blockchain");
 const Block = require("./block");
 describe("Blockchain", () => {
-  let bc;
+  let bc,bc2;
   beforeEach(() => {
     bc = new Blockchain();
+    bc2 = new Blockchain();
   });
   it("starts with a genesis block", () => {
     expect(bc.chain[0]).toEqual(Block.Genesis());
@@ -13,4 +14,8 @@ describe("Blockchain", () => {
     bc.addBock(data);
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
   });
+  it('validates a valid chain',()=>{
+    bc2.addBock('foo');
+    expect(bc.isValidChain(bc2.chain)).toBe(true)
+  })
 });
