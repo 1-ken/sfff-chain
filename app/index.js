747450a57713c107817e4211026041aed1a51f68
KEN <kennicmwimu@gmail.com>
2024-10-28 12:35:17 +0300

    "Added P2P server functionality to app, updated index.js to use P2pServer, and installed ws dependency"

--- app/index.js
+++ app/index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Blockchain = require("../blockchain");
-
+const P2pServer = require("./p2p-server");
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+app.post("/mine", (req, res) => {
+  const block = bc.addBock(req.body.data);
+  console.log(`New Block added ${block.toString()}`);
 
-app.post('/mine',(req,res)=>{
-    const block = bc.addBock(req.body.data)
-    console.log(`New Block added ${block.toString()}`)
-
-    res.redirect('/blocks')
-})
+  res.redirect("/blocks");
+});
 app.get("/blocks", (req, res) => {
   res.json(bc.chain);
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
+p2pServer.listen();
